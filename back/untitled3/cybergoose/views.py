5b0d6a3b17f2fdb6c717d1f4eb65d06968d5da66
Dmitry Paramonov <gamiysikdima@gmail.com>
2017-11-12 18:21:45 +0300

    setup DB
    create model Substance

---
+++ back/untitled3/cybergoose/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+from cybergoose.models import Substance
+
+def index(request):
+    return HttpResponse("Cybergoose\n" + Substance.objects.values()[0]['name'] + ' ' + str(Substance.objects.values()[1]['id']))
