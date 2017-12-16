c6ec68461221065aef111345c0851e72ab85f70b
mmjax <33595233+mmjax@users.noreply.github.com>
2017-12-16 14:08:39 +0300

    com3

---
+++ project/cybergoose/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'', views.test, name='test'),
+    url(r'^feedback/', views.feedback, name='feedback'),
+    url(r'^enter$', views.enter, name='enter'),
+    url(r'^enter/reg$', views.registration, name='registration'),
+    url(r'^aboutus$', views.about_us, name='about_us'),
+    url(r'^enter/reg/visual/', views.visualization, name='visualization'),
+    url(r'^enter/visual/', views.visualization, name='visualization'),
+    url(r'^visual/',)
+
+]
