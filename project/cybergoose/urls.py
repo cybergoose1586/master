925d7db0b73efb7d753a46c75946a2e30953a347
mmjax <33595233+mmjax@users.noreply.github.com>
2017-12-16 14:42:26 +0300

    max

--- project/cybergoose/urls.py
+++ project/cybergoose/urls.py
@@ -1,14 +1,17 @@
 from django.conf.urls import url
+from . impor
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'', views.test, name='test'),
+    url(r'^$', views.test, name='test'),
     url(r'^feedback/', views.feedback, name='feedback'),
     url(r'^enter$', views.enter, name='enter'),
+    url(r'^enter/error$', views.error, name='error'),
     url(r'^enter/reg$', views.registration, name='registration'),
     url(r'^aboutus$', views.about_us, name='about_us'),
     url(r'^enter/reg/visual/', views.visualization, name='visualization'),
     url(r'^enter/visual/', views.visualization, name='visualization'),
-    url(r'^visual/',)
+    url(r'^visual/', views.visualization, name='visualization'),
 
 ]
