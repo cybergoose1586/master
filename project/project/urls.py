df78f8853aaf014bc2ba727845fe753c3aea15d1
mmjax <33595233+mmjax@users.noreply.github.com>
2017-12-16 14:47:33 +0300

    max3

--- project/project/urls.py
+++ project/project/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^cybergoose/', include('cybergoose.urls'))
+    url(r'^cybergoose/', include('project.cybergoose.urls'))
 ]
