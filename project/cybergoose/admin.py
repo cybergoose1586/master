6d3bd68de56cd4960cdffc13409f5dc7151f85c1
mmjax <33595233+mmjax@users.noreply.github.com>
2018-02-19 21:36:17 +0300

    administration & urls

--- project/cybergoose/admin.py
+++ project/cybergoose/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Substance
+
+admin.site.register(Substance)
--- project/cybergoose/models.py
+++ project/cybergoose/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
 class Substance(models.Model):
     name = models.CharField(max_length=30)
@@ -11,6 +11,7 @@ class Substance(models.Model):
     QHeat = models.FloatField(default=0)
 
     def __str__(self):
+        return self.title,
         return ' '.join([
             self.name,
 
@@ -45,3 +46,30 @@ class Experiments(models.Model):
     T2 = models.FloatField(default=0)
     Nheater = models.FloatField(default=0)
     KPDheater = models.FloatField(default=0)
+
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+
+
+
+
+
+
+
+
+
