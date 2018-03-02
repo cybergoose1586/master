ac349256abd4eb2cbcc3ac014a94cc5c2cf57a47
mmjax <33595233+mmjax@users.noreply.github.com>
2018-03-02 22:37:21 +0300

    back all about reg/auth

--- project/cybergoose/urls.py
+++ project/cybergoose/urls.py
@@ -3,15 +3,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^cybergoose', views.start_page, name='test'),
+    url(r'^cybergoose', views.start_page, name='start_page'),
     url(r'^feedback$', views.feedback, name='feedback'),
     url(r'^enter', views.enter, name='enter'),
     url(r'^enter/error$', views.error, name='error'),
-    url(r'^enter/reg$', views.registration, name='registration'),
     url(r'^reg$', views.registration, name='registration'),
     url(r'^aboutus$', views.about_us, name='about_us'),
-    url(r'^enter/reg/visual/', views.visualization, name='visualization'),
-    url(r'^enter/visual/', views.visualization, name='visualization'),
     url(r'^visual/', views.visualization, name='visualization'),
     url(r'^headap', views.HeaderUpdate, name='HeaderUpdate'),
 ]
